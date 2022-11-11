0daa0a55e4367e1940c5af2c552cd68fbc257a60
dwarionas <77571864+dwarionas@users.noreply.github.com>
2022-11-11 11:28:40 +0200

    token hidden

--- my.js
+++ my.js
@@ -2,7 +2,7 @@ const express = require('express')
 const axios = require('axios');
 const app = express()
 const port = 3000
-const token = "mGReORSyXAoi5DA9l8JbyuLZGEZNz9yZ";
+const token = "";
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
